e8bde79d0ca0f244b3a6ca1cd161b07e4c493758
Talha Mansoor <talhaulhaque53@gmail.com>
2026-09-20 16:17:09 +0500

    chore: in_vscode synced with keymaps, comments added for reference

--- lua/in_vscode.lua
+++ lua/in_vscode.lua
@@ -3,6 +3,9 @@
 -- Not named vscode.lua: that would hide the extension's own `require 'vscode'` module.
 -- Built into vscode-neovim already, so not mapped here: K/gh hover, gd definition, gH references,
 -- gO document symbols, gc/gcc comment, zc/zo/za/zM/zR folds.
+-- Still real plugins in VS Code (see the allow-list in init.lua), so also not mapped here:
+-- mini.nvim (<leader>r replace-with-register, mini.ai, mini.surround), flash.nvim (<leader><leader>s),
+-- harpoon (<leader>ha, <leader>hr, <leader>hh, ]h, [h, <leader>1-4).
 local vscode = require 'vscode'
 local map = vim.keymap.set
 
@@ -13,41 +16,43 @@ local function act(cmd)
 end
 
 -- Telescope keys -> VS Code pickers
-map('n', '<leader>ff', act 'workbench.action.quickOpen', { desc = 'Find files' })
-map('n', '<leader>fg', act 'workbench.action.findInFiles', { desc = 'Find by grep' })
-map('n', '<leader>fw', function()
+map('n', '<leader>ff', act 'workbench.action.quickOpen', { desc = 'Find files' }) -- open a file by name
+map('n', '<leader>fg', act 'workbench.action.findInFiles', { desc = 'Find by grep' }) -- search text in the whole project
+map('n', '<leader>fw', function() -- search the word under the cursor
   vscode.action('workbench.action.findInFiles', { args = { { query = vim.fn.expand '<cword>' } } })
 end, { desc = 'Find current word' })
-map('n', '<leader>fs', act 'workbench.action.showCommands', { desc = 'Command palette' })
-map('n', '<leader>fk', act 'workbench.action.openGlobalKeybindings', { desc = 'Find keymaps' })
-map('n', '<leader>fd', act 'workbench.actions.view.problems', { desc = 'Find diagnostics' })
-map('n', '<leader>f.', act 'workbench.action.openRecent', { desc = 'Recent files' })
-map('n', '<leader><leader>', act 'workbench.action.showAllEditors', { desc = 'Open editors' })
-map('n', '<leader>/', act 'actions.find', { desc = 'Find in current file' })
+map('n', '<leader>fs', act 'workbench.action.showCommands', { desc = 'Command palette' }) -- run any VS Code command
+map('n', '<leader>fk', act 'workbench.action.openGlobalKeybindings', { desc = 'Find keymaps' }) -- list VS Code keybindings
+map('n', '<leader>fd', act 'workbench.actions.view.problems', { desc = 'Find diagnostics' }) -- errors and warnings panel
+map('n', '<leader>f.', act 'workbench.action.openRecent', { desc = 'Recent files' }) -- recently opened files/folders
+map('n', '<leader><leader>', act 'workbench.action.showAllEditors', { desc = 'Open editors' }) -- switch between open tabs
+map('n', '<leader>/', act 'actions.find', { desc = 'Find in current file' }) -- search inside this file
 
 -- LSP keys (same as the LspAttach block in plugins.lua) -> VS Code
-map('n', 'grn', act 'editor.action.rename', { desc = 'Rename' })
-map({ 'n', 'x' }, 'gra', act 'editor.action.quickFix', { desc = 'Code action' })
-map('n', 'grr', act 'editor.action.goToReferences', { desc = 'References' })
-map('n', 'gri', act 'editor.action.goToImplementation', { desc = 'Implementation' })
-map('n', 'grd', act 'editor.action.revealDefinition', { desc = 'Definition' })
-map('n', 'grD', act 'editor.action.revealDeclaration', { desc = 'Declaration' })
-map('n', 'grt', act 'editor.action.goToTypeDefinition', { desc = 'Type definition' })
-map('n', 'gW', act 'workbench.action.showAllSymbols', { desc = 'Workspace symbols' })
-map('n', '<leader>=', act 'editor.action.formatDocument', { desc = 'Format file' })
+map('n', 'grn', act 'editor.action.rename', { desc = 'Rename' }) -- rename the symbol everywhere
+map({ 'n', 'x' }, 'gra', act 'editor.action.quickFix', { desc = 'Code action' }) -- quick fix / refactor menu
+map('n', 'grr', act 'editor.action.goToReferences', { desc = 'References' }) -- where is this used
+map('n', 'gri', act 'editor.action.goToImplementation', { desc = 'Implementation' }) -- jump to the implementation
+map('n', 'grd', act 'editor.action.revealDefinition', { desc = 'Definition' }) -- jump to the definition
+map('n', 'grD', act 'editor.action.revealDeclaration', { desc = 'Declaration' }) -- jump to the declaration
+map('n', 'grt', act 'editor.action.goToTypeDefinition', { desc = 'Type definition' }) -- jump to the type
+map('n', 'gW', act 'workbench.action.showAllSymbols', { desc = 'Workspace symbols' }) -- search symbols in the project
+map('n', '<leader>=', act 'editor.action.formatDocument', { desc = 'Format file' }) -- format the whole file (conform in native)
 
 -- git
-map('n', '<leader>gg', act 'workbench.view.scm', { desc = 'Source control' })
-map('n', '<leader>gd', act 'git.openChange', { desc = 'Git diff of this file' })
-map('n', ']g', act 'workbench.action.editor.nextChange', { desc = 'Next git change' })
-map('n', '[g', act 'workbench.action.editor.previousChange', { desc = 'Previous git change' })
+map('n', '<leader>gg', act 'workbench.view.scm', { desc = 'Source control' }) -- Neogit in native
+map('n', '<leader>gd', act 'git.openChange', { desc = 'Git diff of this file' }) -- Diffview in native
+map('n', '<leader>gD', act 'workbench.action.closeActiveEditor', { desc = 'Close git diff' }) -- closes the diff tab again
+map('n', ']g', act 'workbench.action.editor.nextChange', { desc = 'Next git change' }) -- next changed hunk
+map('n', '[g', act 'workbench.action.editor.previousChange', { desc = 'Previous git change' }) -- previous changed hunk
 
 -- windows / panels
-map('n', '<C-h>', act 'workbench.action.navigateLeft', { desc = 'Move focus left' })
-map('n', '<C-l>', act 'workbench.action.navigateRight', { desc = 'Move focus right' })
-map('n', '<C-j>', act 'workbench.action.navigateDown', { desc = 'Move focus down' })
-map('n', '<C-k>', act 'workbench.action.navigateUp', { desc = 'Move focus up' })
-map('n', '<leader>e', act 'workbench.view.explorer', { desc = 'Explorer' })
-map('n', '<leader>np', act 'workbench.action.toggleCenteredLayout', { desc = 'Toggle centred layout' })
-map('n', '<leader>qq', act 'workbench.actions.view.problems', { desc = 'Problems' })
-map('n', '<leader>tr', act 'workbench.action.terminal.toggle', { desc = 'Terminal' })
+map('n', '<C-h>', act 'workbench.action.navigateLeft', { desc = 'Move focus left' }) -- focus the editor group on the left
+map('n', '<C-l>', act 'workbench.action.navigateRight', { desc = 'Move focus right' }) -- focus the editor group on the right
+map('n', '<C-j>', act 'workbench.action.navigateDown', { desc = 'Move focus down' }) -- focus the panel/group below
+map('n', '<C-k>', act 'workbench.action.navigateUp', { desc = 'Move focus up' }) -- focus the group above
+map('n', '<leader>e', act 'workbench.view.explorer', { desc = 'Explorer' }) -- neo-tree in native
+map('n', '<leader>np', act 'workbench.action.toggleCenteredLayout', { desc = 'Toggle centred layout' }) -- no-neck-pain in native
+map('n', '<leader>qq', act 'workbench.actions.view.problems', { desc = 'Problems' }) -- diagnostic list
+map('n', '<leader>tr', act 'workbench.action.terminal.toggle', { desc = 'Terminal' }) -- toggle the terminal panel
+map('n', '<leader>bd', act 'workbench.action.closeActiveEditor', { desc = 'Close buffer' }) -- :bd leaves the tab open in VS Code
